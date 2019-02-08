61032d957e6231551c4a7b5568538e3f61477229
Paul Shapiro <paulshapiro@users.noreply.github.com>
2019-02-08 10:20:31 -0600

    1.1.8

--- assets/js/application.js
+++ assets/js/application.js
@@ -69,8 +69,8 @@ document.addEventListener("DOMContentLoaded", function()
 	const osName = getOS();
 	var mm = mm || {};
 	//
-	var desktopVersionString = '1.1.6'
-	var iosVersionString = '1.1.6'
+	var desktopVersionString = '1.1.8'
+	var iosVersionString = '1.1.8'
 	//
 	var desktopGitHubUrl = 'https://github.com/mymonero/mymonero-app-js/releases'
 	var releasesInfo = 
