7e7560d8b212fb618fa764c3750c146cfa13b350
Kostis Karantias <karantiaskostis@gmail.com>
2019-05-25 14:04:49 +0300

    Point to latest desktop version 1.1.11

--- assets/js/application.js
+++ assets/js/application.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function()
 	const osName = getOS();
 	var mm = mm || {};
 	//
-	var desktopVersionString = '1.1.10'
+	var desktopVersionString = '1.1.11'
 	var iosVersionString = '1.1.10'
 	//
 	var desktopGitHubUrl = 'https://github.com/mymonero/mymonero-app-js/releases'
